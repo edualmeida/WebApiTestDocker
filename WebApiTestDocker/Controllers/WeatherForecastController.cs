70dfb2dccd8a8197f6386424ef9748a7478480cc
Eduardo Almeida <edu.almeida1@gmail.com>
2023-11-20 13:50:11 +0000

    Add project files.

---
+++ WebApiTestDocker/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiTestDocker.Services;
+
+namespace WebApiTestDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var r = new Postgres();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = r.GetValue() //Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebApiTestDocker/Services/Postgres.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+
+namespace WebApiTestDocker.Services
+{
+    public class Postgres
+    {
+        public string GetValue()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=pgsql-dev;User Id=postgres;Password=test1234;Database=postgres;");
+
+            conn.Open();
+
+            // Passing PostGre SQL Function Name
+            NpgsqlCommand command = new NpgsqlCommand("Select colValue from testTable limit 1", conn);
+
+            // Execute the query and obtain a result set
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            // Reading from the database rows
+            List<string> listOfManager = new List<string>();
+            var res = "";
+
+            while (reader.Read())
+            {
+                res = reader["colvalue"].ToString(); // Remember Type Casting is required here it has to be according to database column data type
+                //listOfManager.Add(WSManager);
+            }
+            reader.Close();
+
+            command.Dispose();
+            conn.Close();
+
+            return res;
+        }
+    }
+}
