a5709a3aa5f0b8acdb51018d27a4936322886330
Eduardo <edu.almeida1@gmail.com>
2023-11-30 19:13:00 +0000

    Update dockerbuildstep.sh

--- build/dockerbuildstep.sh
+++ build/dockerbuildstep.sh
@@ -1,3 +1,4 @@
+docker network create tulip-net || true
 docker build -f WebApiTestDocker/Dockerfile -t onur-proj .
 docker rm -f my-webapi
 docker run --restart unless-stopped --name my-webapi --net tulip-net -d -p 5000:80 onur-proj
